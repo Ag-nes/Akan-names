08706d1846b4f75e2513ed64e7bd99a48e6244fe
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2021-11-13 22:14:35 +0300

    created function in scripts.js

---
+++ script.js
@@ -0,0 +1,6 @@
+var akan = function(yy,mm,dd) {
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua","Yaa", "Afua", "Ama"];
+    var maleNames = ["Kwasi", "Kwadwo", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+}
+
