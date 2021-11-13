17326cf182b87e22914186e925d9d2cf88ee2635
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2021-11-13 16:50:20 +0300

    Edited html structure

--- script.js
+++
@@ -1,6 +0,0 @@
-// Back-end
-var akan = function(date,month,year,century,gender) {
-    var maleNNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-    var femaleNNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-}
-}
