36c9204c6b76f658aa37957d4393be171d67952c
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2021-11-14 17:00:51 +0300

    Add script.js structure

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-var akan = function(yy,mm,dd) {
+function validateForm() {
+var dd = parseInt(document.getElementById("birthDay").value);
+var mm = parseInt(document.getElementById("birthMonth").value);
+var yy = parseInt(document.getElementById("birthYear").value);
+var gg = document.querySelector("select[name=Gender]");
+}
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua","Yaa", "Afua", "Ama"];
     var maleNames = ["Kwasi", "Kwadwo", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-}
+    if (gender == "female" && result == 0) {
+        alert(females[0]);
+    }else if(gender == "female" && result == 1) {
+        alert(female[1]);
+    }else if(gender == "female" && result == 2) {
+        alert(female[2]);
+    }else if(gender == "female" && result == 3) {
+        alert(female[3]); 
+    }else if(gender == "female" && result == 4) {
+            alert(female[4]);
+    }else if(gender == "female" && result == 5) {
+        alert(female[5]);
+    }else if(gender == "male" && result == 0) {
+            alert(male[0]);
+    }else if(gender == "male" && result == 1) {
+        alert(male[1]);
+    }else if(gender == "male" && result == 2) {
+        alert(male[2]);
+    }else if(gender == "male" && result == 3) {
+        alert(male[3]);
+    }else if(gender == "male" && result == 4) {
+        alert(male[4]);
+    }else if(gender == "male" && result == 5) {
+        alert(male[5]);
+    }else {
+        alert (null);
+    }          
+            
+
+
 
