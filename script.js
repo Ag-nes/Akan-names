432b7172ca40453b2ae6bfc0841e193235a26b68
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2021-11-14 22:35:22 +0300

    Add modified script.js

--- script.js
+++ script.js
@@ -2,39 +2,27 @@ function validateForm() {
 var dd = parseInt(document.getElementById("birthDay").value);
 var mm = parseInt(document.getElementById("birthMonth").value);
 var yy = parseInt(document.getElementById("birthYear").value);
-var gg = document.querySelector("select[name=Gender]");
+if ((dd <=0) || (dd >31)){
+    alert("Enter the correct date");
 }
+else if ((mm <=0) || (mm >12)){
+    alert("Enter the correct month");
+}
+let CC = yy.value.slice(0,2)
+let YY = yy.value.slice(2,4)
+let MM = mm.value
+let DD = dd.value 
+var targetday = Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7)
+alert(targetday)
+var i=""
+if(targetday==0){
+    i=7
+}
+else{
+    i=targetday-1
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua","Yaa", "Afua", "Ama"];
     var maleNames = ["Kwasi", "Kwadwo", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    if (gender == "female" && result == 0) {
-        alert(females[0]);
-    }else if(gender == "female" && result == 1) {
-        alert(female[1]);
-    }else if(gender == "female" && result == 2) {
-        alert(female[2]);
-    }else if(gender == "female" && result == 3) {
-        alert(female[3]); 
-    }else if(gender == "female" && result == 4) {
-            alert(female[4]);
-    }else if(gender == "female" && result == 5) {
-        alert(female[5]);
-    }else if(gender == "male" && result == 0) {
-            alert(male[0]);
-    }else if(gender == "male" && result == 1) {
-        alert(male[1]);
-    }else if(gender == "male" && result == 2) {
-        alert(male[2]);
-    }else if(gender == "male" && result == 3) {
-        alert(male[3]);
-    }else if(gender == "male" && result == 4) {
-        alert(male[4]);
-    }else if(gender == "male" && result == 5) {
-        alert(male[5]);
-    }else {
-        alert (null);
-    }          
-            
-
-
+    alert("Your Akan name is  "+ femaleNames[i]);    
+}
+}
 
