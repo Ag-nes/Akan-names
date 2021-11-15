27e6f00e0d706b3e862e53de9f44d1da0595b4a8
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2021-11-15 12:28:48 +0300

    Add modified script.js strucure

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-function validateForm() {
-    var dd = parseInt(document.getElementById("birthDay").value);
-    var mm = parseInt(document.getElementById("birthMonth").value);
-    var yy = parseInt(document.getElementById("birthYear").value);
-    if ((dd <=0) || (dd >31)){
+function validate(day, month) {
+    if (day < 1 || day > 31 || month == 2 && day > 29) {
         alert("Enter the correct date");
+        day.focus();
+        return false;
+    } else if (month < 1 || month > 12) {
+        alert("Enter the correct Month");
+        month.focus();
+        return false;
+    } else {
+        return true;
     }
-    else if ((mm <=0) || (mm >12)){
-        alert("Enter the correct month");
-    }
-    let CC = yy.value.slice(0,2)
-    let YY = yy.value.slice(2,4)
-    let MM = mm.value
-    let DD = dd.value 
-    var targetday = Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7)
-    alert(targetday)
-    var i=""
-    if(targetday == 0){
-        i=7
-    }
-    else{
-        i=targetday-1
-        var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua","Yaa", "Afua", "Ama"];
-        var maleNames = ["Kwasi", "Kwadwo", "Kwaku", "Yaw", "Kofi", "Kwame"];
-        var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        alert("Your Akan name is  "+ femaleNames[i]);    
-    }
-    }
-    
-    
+}
+function validateForm() {
+var birthDay = parseInt(document.getElementById("birthDay").value);
+var birthMonth = parseInt(document.getElementById("birthMonth").value);
+var birthYear = parseInt(document.getElementById("birthYear").value);
+var gender = document.querySelector('input[name="Gender"]:checked').value;
+
+let CC = yy.value.slice(0,2)
+let YY = yy.value.slice(2,4)
+let MM = mm.value
+let DD = dd.value 
+var targetday = Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7)
+alert(targetday)
+var i=""
+if(targetday==0){
+    i=7
+}
+else{
+    i=targetday-1
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua","Yaa", "Afua", "Ama"];
+    var maleNames = ["Kwasi", "Kwadwo", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    alert("Your Akan name is  "+ femaleNames[i]);    
+}
+}
+
