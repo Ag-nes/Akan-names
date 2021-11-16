de899942bdbe20622955e00c8cd10316305736b6
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2021-11-16 08:15:59 +0300

    Add changes

--- script.js
+++ script.js
@@ -15,11 +15,13 @@ let YY = yy.value.slice(2,4)
 let MM = mm.value
 let DD = dd.value 
 var targetday = Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7)
+
 var i=""
-if(targetday==0){
+if (targetday==0){
     i=7
 }
 else{
+    
     (i=targetday-1)
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua","Yaa", "Afua", "Ama"];
     var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
